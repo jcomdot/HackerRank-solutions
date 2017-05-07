581047899ec502d8d58bf37191f9cca3941582f6
Jcom. Ltd <jcomdot@gmail.com>
2017-05-07 11:14:35 +0900

    BiConsumer operation. (Java 8)
    Lambda expression and functional interfaces were applied.

--- Java/Data Structures/Java BitSet/Solution.java
+++ Java/Data Structures/Java BitSet/Solution.java
@@ -1,8 +1,6 @@
 import java.io.*;
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
+import java.util.function.BiConsumer;
 
 public class Solution {
 
@@ -11,29 +9,19 @@ public class Solution {
         Scanner in = new Scanner(System.in);
         int N = in.nextInt();
         int M = in.nextInt();
-        BitSet B1 = new BitSet(N);
-        BitSet B2 = new BitSet(N);
+        BitSet[] B = new BitSet[] { new BitSet(N), new BitSet(N) };
+        Map<String, BiConsumer<Integer, Integer>> operator = new HashMap<>();
+        operator.put("AND", (idx1, idx2) -> B[idx1-1].and(B[idx2-1]));
+        operator.put("OR", (idx1, idx2) -> B[idx1-1].or(B[idx2-1]));
+        operator.put("XOR", (idx1, idx2) -> B[idx1-1].xor(B[idx2-1]));
+        operator.put("FLIP", (idx1, idx2) -> B[idx1-1].flip(idx2));
+        operator.put("SET", (idx1, idx2) -> B[idx1-1].set(idx2));
         
         for (int i=0; i<M; i++) {
-            String operator = in.next();
-            int operand1 = in.nextInt();
-            int operand2 = in.nextInt();
-            BitSet b1, b2;
-            if (operand1 == 1) b1 = B1;
-            else b1 = B2;
-            if (operator.equals("AND") || operator.equals("OR") || operator.equals("XOR")) {
-                if (operand2 == 2) b2 = B2;
-                else b2 = B1;
-                if (operator.equals("AND")) b1.and(b2);
-                else if(operator.equals("OR")) b1.or(b2);
-                else if(operator.equals("XOR")) b1.xor(b2);
-            } else {
-                if (operator.equals("SET")) b1.set(operand2);
-                else if (operator.equals("FLIP")) b1.flip(operand2);
-            }
-            
-            System.out.println(String.format("%d %d", B1.cardinality(), B2.cardinality()));
+            operator.get(in.next()).accept(in.nextInt(), in.nextInt());
+            System.out.println(B[0].cardinality() + " " + B[1].cardinality());
         }
+        
         in.close();
     }
 }
